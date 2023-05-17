ccc89bab08e76ae61da8eea9735bda25324107f4
Elian <elianexsequiel@gmail.com>
2023-05-17 20:57:25 -0300

    va perrito primo

---
+++ sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+    createCanvas(400, 400);
+    background(30, 20, 100);
+    frameRate(0.75);
+  }
+  
+  function draw() {
+    var rrr = random(150, 175); //Random Red Rect
+    var rgr = random(150, 175); //RAndom Green Rect
+    var rbr = random(150, 170); //Random Blue Rect
+    var ror = random(50, 200); //Random Opacity Rect
+  
+    var rtr = random(5, 75); //Random Tamaño Rect
+  
+    fill(rrr, rgr, rbr, ror);
+    rect(random(1, 400), random(1, 400), rtr);
+  }
+  
