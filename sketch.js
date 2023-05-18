a943a42718c6409e8f9980e93bdf1058b32c7e67
Elian <elianexsequiel@gmail.com>
2023-05-17 21:40:56 -0300

    ahi te va san pedro

--- sketch.js
+++ sketch.js
@@ -8,6 +8,7 @@ function setup() {
   //nada, aca
   //vos sos del vscode o del navegador?
   //del navegador, vos?
+  //no, yo del vscode. atrrrrrr
   function draw() {
     var rrr = random(150, 175); //Random Red Rect
     var rgr = random(150, 175); //RAndom Green Rect
